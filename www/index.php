aaf680d0fd44bd65ea62f7668e70857c6be6263b
smbanducci <49697561+smbanducci@users.noreply.github.com>
2019-09-25 17:35:10 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,20 @@
-This is a test
+<!doctype html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet"        type="text/css"              href="stylesheet.css">
+  <title>The Whiskful Wanderer</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <header>
+    <nav role="navigation">
+      <h1>
+        <bold>Welcome to the Whiskful Wanderer!</bold>
+      </h1>
+      <img src="Documents/linedWhisks.jpg" alt="Whisks" width="650" height="250">
+      <p>Rants and ravings about recipes, places to eat, and tips and tricks on cooking. </p>
+
+  </header>
+</body>
