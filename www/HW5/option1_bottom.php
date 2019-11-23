a5519cba5fd30b707918af5c777f588c87bd688e
smbanducci <49697561+smbanducci@users.noreply.github.com>
2019-11-23 14:37:57 -0500

    option1_bottom.php
    Apart of HW5 folder that includes: 
    option1.html
    option1.css
    option1.php
    option1_mysqli_connect.php
    option1_top.php

---
+++ www/HW5/option1_bottom.php
@@ -0,0 +1,15 @@
+<!-- CS321: Web-Centric Programming -->
+<!-- Shawna Banducci -->
+<!-- Used code from WC3Schools -->
+
+<!doctype html>
+<html lang="en">
+
+<h2>
+
+<?php
+  echo 'CHEERS!'
+?>
+
+</h2>
+</html>
